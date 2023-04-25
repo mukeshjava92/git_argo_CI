8505a32ed46db9a63658ce487a068164f717ebb9
Mukesh <mukeshjava92@gmail.com>
2023-04-26 00:50:06 +0530

    code change

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ def health():
 @app.route("/")
 def home():
     return "Welcome to Mukesh Youtube Channel  https://www.youtube.com/@mukeshchaudhary8677/featured"
-   
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
 	
